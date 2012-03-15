81b9b22d3c76eb7f9b2b80d5931a56aa07d91c67
Chad Masso <chad.m.masso@gmail.com>
2012-03-15 00:31:50 -0500

    bumping version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = 'espyn',
     packages = ['espyn'],
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'ESPN Python API',
     author='Chad Masso',
     author_email='chad.m.masso@gmail.com',
